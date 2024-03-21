9042a8b10076e0530ba44e011241ab90963df3ea
Cahmarchi95 <cah.de.marchi@hotmail.com>
2024-03-21 16:30:32 -0300

    feat: implementando validações no formulário

--- front-crud-livros/src/app/components/cadastro/cadastro.component.ts
+++ front-crud-livros/src/app/components/cadastro/cadastro.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Livro } from './../../model/Livro';
 import { LivroService } from './../../services/livro.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -9,12 +10,36 @@ import { Router } from '@angular/router';
   styleUrls: ['./cadastro.component.scss'],
 })
 export class CadastroComponent {
+  cadastroForm!: FormGroup;
+  submitted = false;
+
   livro = new Livro();
   livros: Livro[] = [];
 
-  constructor(private livroService: LivroService, private router: Router) {}
+  constructor(
+    private livroService: LivroService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    //validations
+    this.cadastroForm = this.formBuilder.group({
+      titulo: ['', Validators.required],
+      autor: ['', Validators.required],
+      editora: ['', Validators.required],
+      descricao: ['', Validators.minLength(10)],
+      genero: ['', Validators.required],
+      anoLancamento: ['', Validators.required],
+    });
+  }
 
   cadastrarLivros(): void {
+    this.submitted = true;
+    if (this.cadastroForm.invalid) {
+      return;
+    }
+
     this.livroService.cadastrarLivros(this.livro).subscribe((retorno) => {
       this.livros.push(retorno);
       this.livro = new Livro();
